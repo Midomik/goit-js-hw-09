d4ac1f55e06deea6a077515a7f88138365cf2a55
Midomik <80357093+Midomik@users.noreply.github.com>
2023-09-04 10:15:29 +0000

    Deploying to gh-pages from @ Midomik/goit-js-hw-09@94ed5d565f15e2ea161fa2de391e218a4229021a 🚀

--- 03-promises.0cb3597e.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.querySelector(".form"),e=o.querySelector("button"),r=Array.from(o.children);console.log(o),console.log(e),console.log(r),e.addEventListener("click")}();
-//# sourceMappingURL=03-promises.0cb3597e.js.map
