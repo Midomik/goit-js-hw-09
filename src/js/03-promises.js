94ed5d565f15e2ea161fa2de391e218a4229021a
Midomik <krastikrabbs9234@gmail.com>
2023-09-04 13:14:53 +0300

    comm 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,23 +1,71 @@
+import Notiflix from 'notiflix';
+
 const formEl = document.querySelector(".form");
-const submitBtn = formEl.querySelector("button");
-const lablesArr = Array.from(formEl.children); 
 
-// const inputsArr = lablesArr.map(item =>{item.lastChild})
  
-console.log(formEl);
-console.log(submitBtn);
-console.log(lablesArr);
-// console.log(inputsArr);
 
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+function startFunc(e) {
+    e.preventDefault();
+    const { delay, step, amount } = e.currentTarget.elements;
+       
+    for (let i=0; i<=Number(amount.value)-1; i++){
+        const delays= +step.value * i + +delay.value; 
+        let position = i+1;
+        createPromise(position, delays,e).then(()=>{
+            Notiflix.Notify.success(
+                `✅ Fulfilled promise ${position} in ${delays}ms`
+              );
+        }).catch(()=>{
+            Notiflix.Notify.failure(
+                `❌ Rejected promise ${position} in ${delays}ms`
+              );
+        })
+
+    }
+    e.currentTarget.reset();
+}
+
+function createPromise(position, delays, e) {
+    const { delay, step, amount } = e.currentTarget.elements;
+
+    if(delay.value < 0 || step.value < 0 || amount.value < 0){
+        Notiflix.Notify.warning(`Please enter a positive number`);
   }
+  else{
+    
+        console.log(delays);
+      return new Promise((resolve, reject) => {
+          const shouldResolve = Math.random() > 0.3;
+          setTimeout(()=>{
+            if (shouldResolve) {
+                resolve({ position, delays });
+            } else {
+                reject({ position, delays });
+            }
+          }, delays);
+          
+
+      })
+      
+  }
+
+  
+  
 }
 
 
 
-submitBtn.addEventListener("click", )
+formEl.addEventListener("submit", startFunc);
+
+
+
+
+
+
+
+
+
+
+
+
+
